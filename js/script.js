8f9cf834121a0fc78cebf6d88060d6301210d6b6
Acsa <acsa.silveiras@gmail.com>
2025-07-30 01:31:43 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* efeito de escrita na tela inicial */
 const escrito= new Typed('.multiple-text', {
-   strings: ['Técnica em Informática', 'Web Developer', 'Low-code Developer',
-            'Full-stack', 'Futura Cientista de Dados'],
+   strings: ['Técnica em Informática', 'Web Developer', 'Aspirante a Palestrante',
+      'Full-stack','Low-code Developer', 'Futura Cientista de Dados'],
    typeSpeed: 50,
    backSpeed: 50,
    backDelay: 100,
